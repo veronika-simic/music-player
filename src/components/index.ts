6f2189e4c4d5cf5f5a4f213d525c957a6e24a99a
veronika-simic <veronikasimic21@gmail.com>
2023-03-20 08:07:51 +0100

    fix merge conflicts

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import Error from "./Error";
+import Loader from "./Loader";
+import SongCard from "./SongCard";
+
+export {Error, Loader, SongCard};
--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -1,8 +1,13 @@
+import { Error, Loader, SongCard } from '../components';
 
-
-
-export default function Discover(){
-    return (
-        <></>
-    );
+export default function Discover() {
+  const genreTitle = 'Ranchera';
+  return (
+    <div>
+      <div>
+        <h2>Discover {genreTitle}</h2>
+        <select onChange={()=> {}}></select>
+      </div>
+    </div>
+  );
 }
