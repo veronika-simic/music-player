8cabc89d8e70a7c00215cf8434c4ac37fdfcc861
veronika-simic <veronikasimic21@gmail.com>
2023-03-20 08:55:01 +0100

    add genres as select options

---
+++ src/assets/constants.ts
@@ -0,0 +1,11 @@
+export const genres = [
+    {title: 'Ranchera', value: 'RANCHERA'},
+    {title: 'Cumbia', value: 'CUMBIA'},
+    {title: 'Latin pop', value: 'LATIN POP'},
+    {title: ' Norteño', value: ' NORTENO'},
+    {title: 'Corrido', value: 'CORRIDO'},
+    {title: 'Banda', value: 'BANDA'},
+    {title: 'Mariachi', value: 'MARIACHI'},
+    {title: 'Chilena', value: 'CHILENA'},
+    {title: 'Groupera', value: 'GROUPERA'},
+]
--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -1,15 +1,21 @@
 import { Error, Loader, SongCard } from '../components';
-
+import { genres } from '../assets/constants';
 export default function Discover() {
   const genreTitle = 'Ranchera';
   return (
     <div className="flex flex-col bg-purple-800">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10 ">
-        <h2 className='font-bold text-3xl text-white text-left'>Discover {genreTitle}</h2>
-        <select onChange={() => {}} className="bg-white text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5">
-          {}
+        <h2 className="font-bold text-3xl text-white text-left">
+          Discover {genreTitle}
+        </h2>
+        <select
+          onChange={() => {}}
+          className="bg-white text-black-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+        >
+          {genres.map((genre) => (
+            <option>{genre.title}</option>
+          ))}
         </select>
-
       </div>
     </div>
   );
